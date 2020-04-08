e2d842684d5623dad346a353424ba59f7463b69f
pstevic <patrickstevic@outlook.co>
2020-04-08 10:55:44 -0400

    DeliverableTwo

---
+++ pStevicDeliverablesTwo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace pStevicDeliverablesTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nLet's create secret messages!\nGive me a word and I'll encrypt it into a secret code!\n");
+            string inputMessage = Console.ReadLine();
+            string lowercaseValue = inputMessage.ToLower();
+            System.Collections.Generic.List<string> stringList = lowercaseValue.Split("")
+                .ToList();
+
+            int[] encodedMessage = stringList.Select(letter => Enum.Parse(letterValues, letter))
+
+        }    
+    }
+}
---
+++ pStevicDeliverablesTwoValues/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pStevicDeliverablesTwoValues
+{
+    public enum letterValues
+    {
+        a = 1,
+        b,
+        c ,
+        d ,
+        e ,
+        f ,
+        g ,
+        h ,
+        i ,
+        j ,
+        k,
+        l,
+    }
+}
